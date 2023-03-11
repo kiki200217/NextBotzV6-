084319385c3e03f9443bdd30f59e8a804debe017
kiki200217 <124233079+kiki200217@users.noreply.github.com>
2023-03-11 17:35:52 +0700

    Update menu-awal.js

--- plugins/menu-awal.js
+++ plugins/menu-awal.js
@@ -9,7 +9,7 @@ contextInfo: { externalAdReply :{
                         mediaType: 2,
                         description: 'anu',
                         title: bottime,
-                        body: 'AMELIA PUTRI SEPTIANY',          previewType: 0,
+                        body: 'Nextbot',          previewType: 0,
                         thumbnail: fs.readFileSync("./thumbnail.jpg"),
                         sourceUrl: sig
                       }}
